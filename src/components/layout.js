05b3b804ca95654583d02f926c83ab72f19fc534
Iam-VM <vishnumohanantheking@gmail.com>
2022-05-09 14:26:15 +0530

    cleaned layout.js]

---
+++ src/components/layout.js
@@ -0,0 +1,32 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import * as React from "react"
+import PropTypes from "prop-types"
+
+const Layout = ({ children }) => {
+
+  return (
+    <>
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: `var(--size-content)`,
+          padding: `var(--size-gutter)`,
+        }}
+      >
+        <main>{children}</main>
+      </div>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
