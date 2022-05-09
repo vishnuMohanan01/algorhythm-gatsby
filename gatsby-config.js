e8979dd94e976296b0850f07fdd8e21fc301ff6e
Iam-VM <vishnumohanantheking@gmail.com>
2022-05-09 14:24:29 +0530

    updated gatsby config for favico

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Algorhythm22`,
+    description: `Association Day of Computer Science and Engineering, GEC Palakkad.`,
+    author: `Vishnu Mohanan; Anjas Kunnummal; Adhil Muhammed`,
+    siteUrl: `https://www.algorhythm22.tech`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,15 +20,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Algorhythm22`,
+        short_name: `Algorhythm`,
         start_url: `/`,
         background_color: `#663399`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/algorhythm-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
