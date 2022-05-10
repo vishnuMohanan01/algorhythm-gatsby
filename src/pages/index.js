fb87591ae190efc2fb96041acac9816c822fc6d3
Iam-VM <vishnumohanantheking@gmail.com>
2022-05-10 22:29:44 +0530

    using queue based component array

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,41 @@
-import * as React from "react"
+import React, {useState} from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
-import Layout from "../components/layout"
+// importing components and styles
+import About from "../components/about";
+import NavBar from "../components/navBar";
+import PreEvents from "../components/preEvents";
 import Seo from "../components/seo"
-import "../components/index.module.css"
+import "../components/globalStyles/index.module.css"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
+const IndexPage = () => {
+    const [activeComponentID, setActiveComponentID] = useState(0);
+    const indexQueue = [
+        <About setActiveComponentID={setActiveComponentID}/>,
+        <PreEvents setActiveComponentID={setActiveComponentID}/>
+    ];
 
-  </Layout>
-)
+    const incrementActiveComponentID = () => {
+
+    }
+
+    const decrementActiveComponentID = () => {
+
+    }
+
+
+    return (
+        <div style={{width: "100%", height: "100%"}}>
+            <Seo title="Home" />
+            {/*Add NavBar and Active Component here*/}
+            <NavBar activeComponentID={activeComponentID}/>
+            {indexQueue[activeComponentID]}
+        </div>
+    );
+}
+
+
+
 
 export default IndexPage
